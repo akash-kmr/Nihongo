3451ecdbc513315ea06af2c515405472bda14138
akash-kmr <bholuakku@gmail.com>
2018-06-06 19:57:58 +0530

    Added numbers and colors activities.
    Signed-off-by: akash-kmr <bholuakku@gmail.com>

--- app/src/main/java/com/example/android/nihongo/MainActivity.java
+++ app/src/main/java/com/example/android/nihongo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.nihongo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView numbersTextView = (TextView) findViewById(R.id.numbersTextView);
+        numbersTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(intent);
+            }
+        });
+        TextView colorsTextView = (TextView) findViewById(R.id.colorsTextView);
+        colorsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
